e38e2da2dc369cdc8be829dc699a006b0c900e19
Maciej Budrewicz <maciejb@rescompany.com>
2025-01-18 15:44:06 +0100

    Settings up aly

--- Frontend/ordermanagement/src/app/layout/App.tsx
+++ Frontend/ordermanagement/src/app/layout/App.tsx
@@ -2,18 +2,28 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import React from 'react';
 import './styles.css';
 import CustomersDashboard from '../../features/customers/customersDashboard/CustomersDashboard';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './Layout';
+import HomePage from '../../features/home/HomePage';
 
 const client = new ApolloClient({
   cache: new InMemoryCache({
     typePolicies: {}
   }),
-  uri: "http://localhost:5073/graphql/"
+  uri: process.env.REACT_APP_API_SCHEMA_URL
 })
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <CustomersDashboard />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="Customers" element={<CustomersDashboard />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </ApolloProvider>
   );
 }
---
+++ Frontend/ordermanagement/src/features/home/HomePage.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function () {
+    return <h1>Welcom to Order management App</h1>
+}
