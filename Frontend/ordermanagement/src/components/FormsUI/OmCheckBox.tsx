83deb44b5a0b4220a8f9da777c520fa6a292399a
Maciej Budrewicz <maciejb@rescompany.com>
2025-01-20 17:34:58 +0100

    Rest of UI controls

---
+++ Frontend/ordermanagement/src/components/FormsUI/OmCheckBox.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { useField, useFormikContext } from 'formik';
+import { Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, TextField } from "@mui/material";
+
+interface OmCheckBoxProps {
+    name: string,
+    label: string,
+    legend: string,
+    otherProps: any
+}
+export default function OmCheckBox({ name, label, legend, otherProps }: OmCheckBoxProps) {
+    const { setFieldValue } = useFormikContext();
+    const [field, meta] = useField(name);
+
+    function handleChange(event: any) {
+        const { checked } = event.target;
+        setFieldValue(name, checked);
+    }
+
+    const configCheckBox = {
+        ...otherProps,
+        ...field,
+        onChange: handleChange,
+        checked: meta.value
+    }
+
+    const configFormControl: any = {};
+    if (meta && meta.touched && meta.error) {
+        configFormControl.error = true
+
+    }
+
+    return (
+        <FormControl {...configFormControl} >
+            <FormLabel component='legend'>{legend}</FormLabel>
+            <FormGroup>
+                <FormControlLabel
+                    control={<Checkbox {...configCheckBox} />}
+                    label={label}
+                />
+            </FormGroup>
+        </FormControl>
+    );
+
+}
---
+++ Frontend/ordermanagement/src/components/FormsUI/OmDatePicker.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useField, useFormikContext } from 'formik';
+import { MenuItem, TextField } from "@mui/material";
+
+interface OmDatePickerProps {
+    name: string,
+    otherProps: any
+}
+export default function OmDatePicker({ name, otherProps }: OmDatePickerProps) {
+    const [field, meta] = useField(name);
+
+    const configDatePicker = {
+        ...field,
+        ...otherProps,
+        type: 'date',
+        fullWidth: true,
+        variant: 'outlined',
+        InputLabelProps: {
+            shrink: true
+        }
+    }
+
+    if (meta && meta.touched && meta.error) {
+        configDatePicker.error = true;
+        configDatePicker.helperText = meta.error;
+    }
+
+    return (
+        <TextField {...configDatePicker} />
+    )
+}
