6982585037bef794718d3799ccf53288ed882fd7
Maciej Budrewicz <maciejb@rescompany.com>
2025-01-26 12:30:40 +0100

    Create database

--- Backend/API/Program.cs
+++ Backend/API/Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContextFactory<OMAContext>(optons =>
 {
-    optons.UseInMemoryDatabase("InMemoryDb");
+    optons.UseSqlite(builder.Configuration["ConnectionStrings:DefaultConnection"]);
 });
 builder.Services.AddScoped<ICustomerService, CustomerService>();
 builder.Services.AddScoped<IOrderService, OrderService>();
@@ -40,6 +40,27 @@ app.UseCors(AllowSpecificOrigins);
 app.MapGraphQL();
 
 app.UseGraphQLVoyager("/graphql-voyager", new VoyagerOptions { GraphQLEndPoint = "/graphql" });
+
+//Migrate database
+try
+{
+    var scope = app.Services.CreateScope();
+    var context = scope.ServiceProvider.GetRequiredService<OMAContext>();
+    context.Database.Migrate();
+    //using (var scope = app.Services.CreateScope())
+    //{
+    //    var dbFactory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<OMAContext>>();
+    //    using (var context = dbFactory.CreateDbContext())
+    //    {
+    //        context.Database.Migrate();
+    //    }
+    //}
+}
+catch (Exception ex)
+{
+    var logger = app.Services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred while migrating the database.");
+}
 
 app.Run();
 
